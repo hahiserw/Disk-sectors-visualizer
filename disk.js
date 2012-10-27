b9be3c0af81cbd10c5021efee12fcefe5d591026
Maurycy Skier <hahiserw@google.com>
2012-10-27 21:37:16 +0200

    Checkpoint.

--- disk.js
+++ disk.js
@@ -1,22 +1,118 @@
 /*
  * Disk class for Disk sectors visualizer
+ *
+ * Draws blocks in grid onto the canvas with podanym poprzednio context
  */
 
 
-var Disk = function( LBA, blockSize, data ) {
+var Disk = function( LBA, blockSize, data, context ) {
 	this.lba = LBA;
 	this.blockSize = blockSize;
 	this.data = data;
+	this.context = context;
+	this.offset = 0;
 };
 
-Disk.prototype.sizes = function( width, height, blockWidth, blockHeight ) {
-	this.width = width;
-	this.height = height;
+Disk.prototype.sizes = function( width, height, blockWidth, blockHeight, blockMargin ) {
+	this.width = width - width % blockWidth;
+	this.height = height - height % blockHeight;
 	this.blockWidth = blockWidth;
 	this.blockHeight = blockHeight;
-}
+	this.margin = blockMargin;
+	this.blocksPerLine = Math.floor( width / blockWidth );
+	this.blocksPerPage = Math.floor( height / blockHeight )
+};
 
-Disk.prototype.visualize = function( context ) {
+Disk.prototype.moveOffset = function( x, y, page ) {
+	this.offset +=
+		x +
+		y * this.blocksPerLine +
+		page * this.blocksPerPage * this.blocksPerLine;
+};
+
+// Draws fragment of map according to start block
+Disk.prototype.visualize = function() {
+
+	// colors: oh god (bad block), not that bad, not good, damn
+	var
+		codes = [ "#000000", "#008000", "#ff8000", "#ff0000" ],
+		blockWidth = this.blockWidth + this.margin,
+		blockHeight = this.blockHeight + this.margin;
+	var
+		width = blockWidth * this.blocksPerLine,
+		height = blockHeight * this.blocksPerPage;
+
+	// Set drawing style for board
+	this.context.save();
+	this.context.fillStyle = "#f5f5f5";
+	this.context.strokeStyle = "#ffffff";
+	this.context.fillRect( 0, 0, width, height );
+
+	// Draw grid
+	if( this.margin > 0 ) {
+		
+		this.context.save();
+		this.context.fillStyle = "#ffffff";
+		
+		// Draw vertical lines
+		for( var x = 0; x < this.blocksPerLine; x++ )
+			this.context.fillRect( x * blockWidth, 0, this.margin, height );
+
+		// Draw horizontal lines
+		for( var y = 0; y < this.blocksPerPage; y++ )
+			this.context.fillRect( 0, y * blockHeight, width, this.margin );
+
+		this.context.restore();
+	}
+
+	// Put every block onto the canvas
 	for( var i = 0; i < this.data.length; i++ )
-		;
-}
+	{
+		// Skip to start block
+		if( this.data[i] < this.offset * this.blockSize )
+			continue;
+
+		// Draw a block based on index and access time
+		var index = Math.floor( this.data[i] / this.blockSize );
+		var color = this.data[i] % this.blockSize;
+
+		// Compute positions
+		var x = ( index - this.offset ) % this.blocksPerLine;
+		var y = Math.floor( ( index - this.offset ) / this.blocksPerLine );
+
+		// Stop drawing when end of board is reached
+		if( y >= this.blocksPerPage )
+			break;
+
+		// Draw block
+		this.context.save();
+		this.context.fillStyle = codes[color];
+		this.context.fillRect( x * blockWidth + this.margin, y * blockHeight + this.margin,
+			this.blockWidth, this.blockHeight );
+		this.context.restore();
+	}
+
+	this.context.restore();
+
+};
+
+// Converts data array to text format
+Disk.prototype.textualize = function( start ) {
+
+	// 'Colors'
+	var codes = [ "#", "+", "%", "&" ];
+	// bacground = ".";
+
+	for( var i = 0; i < this.data.length; i++ )
+	{
+		//
+	}
+
+};
+
+/*
+
+		var base = ( index - start ) * ( this.blockWidth + this.margin );
+		var x = ( base % this.width ) + this.margin;
+		var y = Math.floor( base / this.width )	* ( this.blockHeight + this.margin ) + this.margin;
+*/
--- parser.js
+++ parser.js
@@ -1,62 +1,81 @@
 /*
  * Parser for Disk sectrors visualizer
+ *
+ * Converts HDDscan for Win2k/XP format to array of blocks with colors.
+ * "Block start at 47616 time 203ms" -> 47616 + 2 (block + color)
  */
 
 
 var Parser = function() {
-
 	this.header_length = 9;
 	this.header = this.nop;
 	this.data = this.nop;
 	this.error = this.nop;
-
 };
 
 
-Parser.prototype.feed = function( text ) {
+Parser.prototype.feed = function( text, onlyBadBlocks ) {
+
+	onlyBadBlocks = !!onlyBadBlocks;
 
 	var
 		data,
-		line,
-		lines = [],
 		header = {},
-		convert = /^(.+)\s+:\s+(.+?)\s*$/,
-		block = /^Block start at (\d+) time (\d+)ms$/,
-		badBlock = /^Bad block found, start LBA : (\d+)$/;
+		re = {
+			header: /^(.+)\s+:\s+(.+?)\s*$/,
+			block: /^Block start at (\d+) time (\d+)ms$/,
+			badBlock: /^Bad block found, start LBA : (\d+)$/
+		};
 
 	// A lot of memory for a large text...
-	lines = text.split( "\n" );
+	var lines = text.split( "\n" );
 
 	
 	// First 8 lines are a header.
 
-	header["About"] = lines[0];
+	header["About"] = lines[0]; // No blank lines before header... Not good.
 
 	for( var i = 1; i < this.header_length; i++ )
 	{
-		data = lines[i].match( convert );
-		if( data !== null )
-			header[data[1]] = data[2];
+		data = lines[i].match( re.header );
+		if( data !== null ) {
+			var value = data[2];
+			if( /^\d+$/.test( value ) )
+				value = parseInt( data[2] );
+			header[data[1]] = value;
+		}
 	}
 
 	this.header( header );
 
 	
 	data = [];
-	var line;
 
-	for( var i = 0; i < lines.length - this.header_length; i++ )
+	for( var i = this.header_length, j = 0; i < lines.length; i++ )
 	{
-		line = lines[i+this.header_length].match( block );
-		// Block start at \d+ time \d+ms
+		var line = lines[i].match( re.badBlock );
+		// Bad block found, start LBA : \d+
 		if( line !== null ) {
-			data[i] = [ line[1], line[2] ];
-		} else {
-			// Bad block found, start LBA : \d+
-			line = lines[i].match( badBlock );
-			if( line !== null )
-				data[i] = [ line[1], 0 ];
-		}
+			data[j++] = parseInt( line[1] );
+		} else
+			if( !onlyBadBlocks ) {
+				line = lines[i].match( re.block );
+				// Block start at \d+ time \d+ms
+				if( line !== null ) {
+					// Chose color depending on the access time
+					var color = 0;
+					var access = parseInt( line[2] );
+					if( access !== 0 ) {
+						color++;
+						if( 150 < access )
+							color++;
+						else
+							if( 500 < access )
+								color++;
+					}
+					data[j++] = parseInt( line[1] ) + color;
+				}
+			}
 	}
 
 	this.data( data );
